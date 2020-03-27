4e45bfdbe81e476265848db4c24bc98b8b894311
fym1 <1668576168@qq.com>
2020-03-27 09:14:07 +0800

    fym

--- 43-timer-button/timer-button.js
+++ 43-timer-button/timer-button.js
@@ -35,44 +35,6 @@ define(['jquery'],function($){
         } 
     }
     return TimerButton;
-    // var $timerButton=(function(){
-    // function show(conf) {
-    //     var $btn=$('<input class="timer-button" type="button" value="同意(6s)" disabled />');
-
-    //     var timer;
-    //     var num;
-    //     var cfg={
-    //         container: "body",
-    //         num: 6,
-    //         title: "同意",
-    //         onClick:null
-    //     }
-    //     //1.DOM draw
-    //     if (timer) clearInterval(timer);
-
-    //     $(cfg.container).append($btn);
-    //     $.extend(cfg,conf);
-    //     num = cfg.num;
-    //     $btn.val(cfg.title+"("+num+"s)")
-    //     timer =setInterval(function() {
-    //         num--;
-    //         if(num===0){
-    //             clearInterval(timer);
-    //             $btn.val(cfg.title);
-    //             $btn.removeAttr("disabled");
-    //         }
-    //         else{
-    //             $btn.val( cfg.title+"("+num+"s)");
-    //         }
-    //     }, 1000);
-    //     //2.event bind
-    //     $btn.click(cfg.onClick());
-    // }
-    
-    // return {
-    //     show: show
-    // }   
-    // }());
 })   
     
     //不用 page load event
